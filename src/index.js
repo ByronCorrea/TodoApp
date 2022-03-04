fe5de3221e6d10776c435ec283721c2755869646
ByronCorrea <byron10-@hotmail.com>
2022-03-04 01:01:49 -0300

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./containers/App";
-import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
