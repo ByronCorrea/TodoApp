d3db5f11157d76548376c861d7b71b154bdf4676
ByronCorrea <byron10-@hotmail.com>
2022-03-05 00:22:37 -0300

    Create components
    Create components and add readme with preview of proyect

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,36 @@
+import React, { useState } from "react";
 import "./App.css";
+import TodoList from "../components/TodoList.jsx";
 
 function App() {
-  return <div className="App"></div>;
+  const [darkMode, setDarkMode] = useState(false);
+  return (
+    <div className={darkMode ? "dark-mode" : "light-mode"}>
+      {/* <div>
+        <h3>{darkMode ? "DARK" : "LIGHT"} MODE</h3>
+      </div> */}
+      <div className="container">
+        <span style={{ color: darkMode ? "#e9ecef" : "yellow" }}>
+          <i class="bx bxs-sun"></i>
+        </span>
+        <div className="switch-checkbox">
+          <label className="switch">
+            <input type="checkbox" onChange={() => setDarkMode(!darkMode)} />
+            <span className="slider round"></span>
+          </label>
+        </div>
+        <span style={{ color: darkMode ? "yellow" : "#e9ecef" }}>
+          <i class="bx bxs-moon"></i>
+        </span>
+      </div>
+      <div
+        className="App"
+        style={{ background: darkMode ? "#343a40" : "#003049" }}
+      >
+        <TodoList />
+      </div>
+    </div>
+  );
 }
 
 export default App;
